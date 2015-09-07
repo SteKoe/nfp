9f677b6ed9db6d02532f263b178e02723d972eab
Stephan Köninger <github@stekoe.de>
2015-09-07 07:46:01 +0200

    chore(test): reenables all tests

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # NFP - D3.js based chart
 
+[![Build Status](https://travis-ci.org/SteKoe/nfp.svg?branch=master)](https://travis-ci.org/SteKoe/nfp)
+
 ![Current State](screenshot.png)
--- app/js/core/services/symptothermalMethod.spec.js
+++ app/js/core/services/symptothermalMethod.spec.js
@@ -10,7 +10,7 @@ describe('SymptoThermalMethod', function() {
         Cervix = _Cervix_;
     }));
 
-    fit('example cycle 1.', function() {
+    it('example cycle 1.', function() {
         var measurements = [
             {temperature: 36.50, cervix: undefined},
             {temperature: 36.35, cervix: undefined},
